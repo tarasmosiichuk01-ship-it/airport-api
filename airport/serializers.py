8f36536cc3e27f0e86e3b58e2723f65a58accd9f
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-04-09 15:18:50 +0300

    Create serializers for all models

---
+++ airport/serializers.py
@@ -0,0 +1,68 @@
+from rest_framework import serializers
+
+from airport.models import (
+    Airport,
+    AirplaneType,
+    Crew,
+    Route,
+    Airplane,
+    Flight,
+    Order,
+    Ticket
+)
+
+
+class AirportSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Airport
+        fields = ("id", "name", "closest_big_city")
+
+
+class AirplaneTypeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = AirplaneType
+        fields = ("id", "name")
+
+
+class CrewSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Crew
+        fields = ("id", "first_name", "last_name")
+
+
+class RouteSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Route
+        fields = ("id", "source", "destination", "distance")
+
+
+class AirplaneSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Airplane
+        fields = ("id", "name", "rows", "seats_in_row", "airplane_type", "capacity")
+
+
+class FlightSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Flight
+        fileds = ("id", "route", "airplane", "crew", "departure_time", "arrival_time")
+
+
+class OrderSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Order
+        fields = ("id", "created_at", "user")
+
+
+class TicketSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Ticket
+        fields = ("id", "row", "seat", "flight", "order")
