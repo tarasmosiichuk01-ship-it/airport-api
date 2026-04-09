94fd476bc8b95b931b8e6ce37c3d1c4cb933ed1f
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-04-09 13:25:15 +0300

    Add model Crew to airport\models.py

--- airport/models.py
+++ airport/models.py
@@ -14,3 +14,15 @@ class AirplaneType(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
+
+    @property
+    def full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self) -> str:
+        return self.full_name
