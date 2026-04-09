9e2693c9bfba1ad5629907f5b754c69c503e1939
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-04-09 15:34:26 +0300

    Create and define urls for airport views

---
+++ airport/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from airport.views import AirportViewSet, AirplaneTypeViewSet, CrewViewSet, RouteViewSet, FlightViewSet, OrderViewSet, \
+    TicketViewSet
+
+app_name = "airport"
+
+router = routers.DefaultRouter()
+router.register("airports", AirportViewSet)
+router.register("airplane-types", AirplaneTypeViewSet, basename="airplane-type")
+router.register("crews", CrewViewSet)
+router.register("routes", RouteViewSet)
+router.register("airplanes", AirportViewSet)
+router.register("flights", FlightViewSet)
+router.register("orders", OrderViewSet)
+router.register("tickets", TicketViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
