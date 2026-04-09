a8fd5cb8dfc30546f0850d0dc66b8634815b8a4f
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-04-09 15:25:59 +0300

    Create views for all airport\models

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from airport.models import (
+    Airport,
+    AirplaneType,
+    Crew,
+    Route,
+    Airplane,
+    Flight,
+    Order,
+    Ticket
+)
+
+from airport.serializers import (
+    AirportSerializer,
+    AirplaneTypeSerializer,
+    CrewSerializer,
+    RouteSerializer,
+    AirplaneSerializer,
+    FlightSerializer,
+    OrderSerializer,
+    TicketSerializer
+)
+
+
+class AirportViewSet(viewsets.ModelViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+
+class AirplaneTypeViewSet(viewsets.ModelViewSet):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+
+class AirplaneViewSet(viewsets.ModelViewSet):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
+
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
