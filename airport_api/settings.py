c22e212b1ae7a3b297939d27ab18e93c45820402
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-04-10 15:40:48 +0300

    Add pagination to settings.py

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -127,3 +127,8 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+   "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+   "PAGE_SIZE": 3
+}
